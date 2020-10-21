9132979578ca91a7e9e6de1cacb476ed1ddecf7c
NikolayS93 <nikolays93@ya.ru>
2020-10-21 22:47:32 +0400

    Route name

--- 1. Routes.php
+++ 1. Routes.php
@@ -15,9 +15,21 @@
 use App\Http\Controllers\ArticleController;
 
 // Добавить запрос /article (запускать метод index контроллера ArticleController)
-Route::get('article/', [ArticleController::class, 'index']);
+Route::get('article/', [ArticleController::class, 'index'])->name('article.list');
 
 /** @var array Методы запросов */
 $methods = ['get', 'post', 'put', 'patch', 'delete', 'options'];
 // Добавить запрос с методами и отправить параметр в функцию/метод (к примеру /article/2 вернет 2)
-Route::match($methods, 'article/{id}', function ($id) { return $id; });
+Route::match($methods, 'article/{id}', function ($id) { return $id; })->name('article.show');
+
+/**
+ * Получить ссылку на статью
+ *
+ * @param  string $id ID статьи ссылку на которую нужно получить
+ * @return string     Ссылка на статью к пр. http://example.com/post/1?search=rocket
+ */
+function getArticlePermalink($id) {
+    // Сгенерировать и вернуть ссылку запроса с именем article.show
+    // Если передать не входящий в запрос параметр (search) он добавится как get запрос
+    return route('article.show', ['id' => $id, 'search' => 'rocket']);
+}

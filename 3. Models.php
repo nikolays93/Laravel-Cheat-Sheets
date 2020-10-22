41145cc35a8a20f93909d2c43012460bfb05b4db
NikolayS93 <nikolays93@ya.ru>
2020-10-22 22:56:44 +0400

    Model file

---
+++ 3. Models.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @url https://laravel.com/docs/8.x/eloquent#defining-models
+ * @file app/Models/Article.php
+ *
+ * Создать модель Article с миграциями
+ * $ php artisan make:model Article -m
+ */
